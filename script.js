f3daf1cd7d4105326be2559c5d788da936e864c8
Gideon Goddard <goddard.gcw@gmail.com>
2019-11-18 15:01:34 +0000

    Added header, instructions and good luck message, plus styling for all elements.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+// Setting variables for each of the door elements
 let doorImage1 = document.getElementById("door1");
+let doorImage2 = document.getElementById("door2");
+let doorImage3 = document.getElementById("door3");
+
+// Setting the path for the replacement door images
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+
+// Door click functions
 doorImage1.onclick = () => {
   doorImage1.src = botDoorPath;
+}
+doorImage2.onclick = () => {
+  doorImage2.src = beachDoorPath;
+}
+doorImage3.onclick = () => {
+  doorImage3.src = spaceDoorPath;
 }
