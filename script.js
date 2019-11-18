31ea21c15d7b66806e72925eab4658d7411e8107
Gideon Goddard <goddard.gcw@gmail.com>
2019-11-18 15:23:59 +0000

    Created random door generator function.

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ let doorImage1 = document.getElementById("door1");
 let doorImage2 = document.getElementById("door2");
 let doorImage3 = document.getElementById("door3");
 
+// Random door generator
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+let randomChoreDoorGenerator = () => {
+  let choreDoor = Math.floor(Math.random() * numClosedDoors);
+  if (choreDoor == 0) {
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  } else if (choreDoor == 1) {
+    openDoor1 = spaceDoorPath;
+    openDoor2 = botDoorPath;
+    openDoor3 = beachDoorPath;
+  } else if (choreDoor == 2) {
+    openDoor1 = beachDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor3 = botDoorPath;
+  }
+}
+
 // Setting the path for the replacement door images
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
@@ -10,11 +32,14 @@ const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chor
 
 // Door click functions
 doorImage1.onclick = () => {
-  doorImage1.src = botDoorPath;
+  doorImage1.src = openDoor1;
 }
 doorImage2.onclick = () => {
-  doorImage2.src = beachDoorPath;
+  doorImage2.src = openDoor2;
 }
 doorImage3.onclick = () => {
-  doorImage3.src = spaceDoorPath;
+  doorImage3.src = openDoor3;
 }
+
+// Calling the random door generator
+randomChoreDoorGenerator();
